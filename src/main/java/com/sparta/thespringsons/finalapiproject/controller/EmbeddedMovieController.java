5c96b5d83ad06bfdbfc411eb6ae9eab584d4ef24
Affiq <affiqrbk@gmail.com>
2023-12-04 16:53:08 +0000

    Added more dummy APIs and Swagger UI

--- src/main/java/com/sparta/thespringsons/finalapiproject/controller/EmbeddedMovieController.java
+++ src/main/java/com/sparta/thespringsons/finalapiproject/controller/EmbeddedMovieController.java
@@ -2,8 +2,7 @@ package com.sparta.thespringsons.finalapiproject.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmbeddedMovieController {
@@ -13,6 +12,42 @@ public class EmbeddedMovieController {
     @GetMapping("/embeddedMovies")
     public void getAllEmbeddedMovies() {
         // Gets all embedded movie
+        // Should return list.
     }
 
+    @Tag(name = "Embedded Movie API")
+    @Operation(summary = "Gets an embedded movie by its ID.")
+    @GetMapping("/embeddedMovie/{movie_id}")
+    public void getEmbeddedMovieById(@PathVariable String movie_id) {
+        // Gets embedded movie by id
+        // Should return Optional.
+    }
+
+    @Tag(name = "Embedded Movie API")
+    @Operation(summary = "Gets an embedded movie by date range")
+    @GetMapping("/embeddedMovie/")
+    public void getEmbeddedMovieByDateRange
+            (@RequestParam Integer lowerYear, @RequestParam Integer upperYear) {
+        // Gets an embedded movie by date range
+        // If either are empty or null, it assumes to search everything before or after
+        // Should return a list.
+    }
+
+    @Tag(name = "Embedded Movie API")
+    @Operation(summary = "Creates a new EmbeddedMovie record into the database.")
+    @PostMapping("/embeddedMovie")
+    public void createEmbeddedMovie() { // We need a body in here somewhere
+        // Gets all embedded movie
+        // Maybe returns the JSON body of created object?
+    }
+
+    @Tag(name = "Embedded Movie API")
+    @Operation(summary = "Gets alL embedded movie by runtime range")
+    @GetMapping("/embeddedMovies")
+    public void getEmbeddedMovieByRuntimeRange
+            (@RequestParam Integer lowerRuntime, @RequestParam Integer upperRuntime){
+        // Gets all embedded movies within the specified runtime range
+        // If either are empty or null, it assumes to search everything before or after
+        // Returns a list
+    }
 }
