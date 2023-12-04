780f05fa37c37a0d2d595f91101e1882a481aa97
HenriqueMCunha <hcunha@spartaglobal.com>
2023-12-04 15:52:22 +0000

    Added getFilmByTitle method

--- src/main/java/com/sparta/thespringsons/finalapiproject/controller/MovieController.java
+++ src/main/java/com/sparta/thespringsons/finalapiproject/controller/MovieController.java
@@ -1,4 +1,30 @@
 package com.sparta.thespringsons.finalapiproject.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MovieController {
+
+
+    /*
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieController(MovieRepository movieRepository) {
+        this.movieController = movieController;
+    }
+
+    //Partial search for movie title
+    @GetMapping("/movies")
+    public void getFilmByTitle(@Requestparam(name = "title", required = true) String title) {
+        Optional<MovieDTO> movie = movieRepository.findByTitle();
+
+        if(movie.isPresent() && movie.getTitle().contains(title)) {
+            return movie;
+        }
+    }
+
+
+
+     */
 }
