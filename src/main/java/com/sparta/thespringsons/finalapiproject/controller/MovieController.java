8fa2c806d213250c01aa74635258502e4735414e
HenriqueMCunha <hcunha@spartaglobal.com>
2023-12-04 16:11:09 +0000

    Added getMoviesByPerformer, getMoviesByGenre and getMoviesByDirector methods to MovieController

--- src/main/java/com/sparta/thespringsons/finalapiproject/controller/MovieController.java
+++ src/main/java/com/sparta/thespringsons/finalapiproject/controller/MovieController.java
@@ -15,14 +15,68 @@ public class MovieController {
     }
 
     //Partial search for movie title
-    @GetMapping("/movies")
+    @Tag(name = "Movie Controller API")
+    @Operation(summary = "")
+    @GetMapping("/movies/title")
     public void getFilmByTitle(@Requestparam(name = "title", required = true) String title) {
-        Optional<MovieDTO> movie = movieRepository.findByTitle();
+        //return lists instead of optional
+        Optional<MovieDTO> movie = movieRepository.findByTitle(title);
 
         if(movie.isPresent() && movie.getTitle().contains(title)) {
             return movie;
         }
     }
+
+    @GetMapping("/movies/director")
+    public void getFilmByDirector(@Requestparam(name = "director", required = true) String director) {
+        Optional<MovieDTO> movie = movieRepository.findByDirector(director);
+
+        if(movie.isPresent() && movie.getDirector().contains(director)) {
+            return movie;
+        }
+    }
+
+    @GetMapping("/movies/performer")
+    public void getFilmByPerformer(@Requestparam(name = "performer", required = true) String performer) {
+        List<MovieDTO> movies = movieRepository.findAllMoviesByActors(performer);
+
+        if(movie.isPresent() && movie.getActors().contains(performer)) {
+            return movies;
+        } else {
+            return new PerformerNotFoundException(performer);
+        }
+    }
+
+    @GetMapping("/movies/genre")
+    public void getFilmByGenre(@Requestparam(name = "genre", required = true) String genre) {
+        List<MovieDTO> movies = movieRepository.findAllMoviesByGenre(genre);
+
+        if(movie.isPresent() && movie.getGenre().contains(genre)) {
+            return movies;
+        } else {
+            return new GenreNotFoundException(performer);
+        }
+    }
+
+    @GetMapping("/movies/genre")
+    public void getFilmByGenre(@Requestparam(name = "genre", required = true) String genre) {
+        List<MovieDTO> movies = movieRepository.findAllMoviesByGenre(genre);
+
+        if(movie.isPresent() && movie.getGenre().contains(genre)) {
+            return movies;
+        } else {
+            return new GenreNotFoundException(performer);
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
 
