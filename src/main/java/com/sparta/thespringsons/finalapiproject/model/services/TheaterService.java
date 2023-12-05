4f219e5eee312ece4c91fcb61d64252034a5bdb1
Affiq <affiqrbk@gmail.com>
2023-12-05 14:22:39 +0000

    Added TheaterService with getAll and getById

---
+++ src/main/java/com/sparta/thespringsons/finalapiproject/model/services/TheaterService.java
@@ -0,0 +1,29 @@
+package com.sparta.thespringsons.finalapiproject.model.services;
+
+import com.sparta.thespringsons.finalapiproject.model.entities.Theater;
+import com.sparta.thespringsons.finalapiproject.model.repositories.TheatersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TheaterService {
+
+    private final TheatersRepository theatersRepository;
+
+    @Autowired
+    public TheaterService(TheatersRepository theatersRepository) {
+        this.theatersRepository = theatersRepository;
+    }
+
+    public List<Theater> getAllTheaters() {
+        return theatersRepository.findAll();
+    }
+
+    public Optional<Theater> getTheaterById(String theater_id) {
+        return theatersRepository.findById(theater_id);
+    }
+
+}
