8ab55896403af2e14e6b80b1ebe32413687272d1
Affiq <affiqrbk@gmail.com>
2023-12-06 21:47:52 +0000

    Fixed merge conflicts

--- src/main/java/com/sparta/thespringsons/finalapiproject/security/AuthenticationService.java
+++ src/main/java/com/sparta/thespringsons/finalapiproject/security/AuthenticationService.java
@@ -4,13 +4,10 @@ import com.sparta.thespringsons.finalapiproject.model.repositories.ApiKeyReposit
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
-<<<<<<< HEAD
 import org.springframework.stereotype.Service;
-=======
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.authority.AuthorityUtils;
->>>>>>> ae17f5c0ee9383c4937d9494582c68af9a50ac9b
 
+
+@Service
 public class AuthenticationService {
 
     private final ApiKeyRepository apiKeyRepository;
@@ -20,24 +17,16 @@ public class AuthenticationService {
         this.apiKeyRepository = apiKeyRepository;
     }
 
-<<<<<<< HEAD
     public boolean checkUserAccess(HttpServletRequest request) throws Exception {
         // Allow requests without an API key for HTTP GET methods
-=======
-    public static ApiKeyAuthentication getAuthentication(HttpServletRequest request) {
-        String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
-
->>>>>>> ae17f5c0ee9383c4937d9494582c68af9a50ac9b
         if (HttpMethod.GET.matches(request.getMethod())) {
             return false;
             // Maybe we want to throw status code or exception here instead
         }
-<<<<<<< HEAD
+
         // For all other methods (POST, PUT, DELETE, etc.), require a valid API key
         String apiKey = request.getHeader("Key");
-=======
 
->>>>>>> ae17f5c0ee9383c4937d9494582c68af9a50ac9b
         if (apiKey == null || !isValidApiKey(apiKey)) {
             throw new Exception("Invalid API Key");
         }
@@ -45,15 +34,8 @@ public class AuthenticationService {
         return true;
     }
 
-<<<<<<< HEAD
+
     private boolean isValidApiKey(String apiKey) {
-=======
-    private static boolean isValidApiKey(String apiKey) {
-        if (apiKeyRepository == null) {
-            throw new IllegalStateException("apiKeyRepository is not initialized");
-        }
-        // Check if the API key is present in the database
->>>>>>> ae17f5c0ee9383c4937d9494582c68af9a50ac9b
         return apiKeyRepository.findByApiKey(apiKey).isPresent();
     }
 }
