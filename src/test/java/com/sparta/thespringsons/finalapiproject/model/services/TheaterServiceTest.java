7ec0adaffe231f4df7d9c773cfe9a069b6ebc653
Affiq <affiqrbk@gmail.com>
2023-12-08 00:07:56 +0000

    More testing cov. for theater service

--- src/test/java/com/sparta/thespringsons/finalapiproject/model/services/TheaterServiceTest.java
+++ src/test/java/com/sparta/thespringsons/finalapiproject/model/services/TheaterServiceTest.java
@@ -6,10 +6,7 @@ import com.sparta.thespringsons.finalapiproject.model.fields.Geo;
 import com.sparta.thespringsons.finalapiproject.model.fields.Location;
 import jakarta.validation.constraints.AssertTrue;
 import org.junit.FixMethodOrder;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,24 +17,20 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@FixMethodOrder(MethodSorters.DEFAULT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TheaterServiceTest {
 
     @Autowired
     private TheaterService theaterService;
 
-    @BeforeAll
-    public static void setUp(@Autowired TheaterService theaterService) throws Exception {
-        // Init test data
-        float[] coords = {100.27F, 0.87F};
-        Geo geo = new Geo(coords, "Point");
-        Address address = new Address("TestCity", "TestState","TestSt1","1234567");
-        Location location = new Location(geo,address);
-        Theater theater = new Theater(null, location, 9999999);
-        theaterService.saveTheater(theater);
-    }
+    float[] coords = {100.27F, 0.87F};
+    Geo geo = new Geo(coords, "Point");
+    Address address = new Address("TestCity", "TestState","TestSt1","1234567");
+    Location location = new Location(geo,address);
+    Theater theaterTestRec = new Theater(null, location, 9999999);
 
     @Test
+    @Order(1)
     @DisplayName("Testing get all theaters method has more than 5 theaters")
     public void getAllTheaterTests() {
         List<Theater> theaterList = theaterService.getAllTheaters();
@@ -46,6 +39,7 @@ class TheaterServiceTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("Testing get theater by theaterID")
     public void getTheaterByTheaterId() {
         Optional<Theater> testTheater = theaterService.getTheaterByTheaterId(1000);
@@ -55,6 +49,7 @@ class TheaterServiceTest {
     }
 
     @Test
+    @Order(3)
     @DisplayName("Testing get theater by ID")
         public void getTheaterById() {
         Optional<Theater> testTheater = theaterService.getTheaterById("59a47286cfa9a3a73e51e72c");
@@ -64,6 +59,7 @@ class TheaterServiceTest {
     }
 
     @Test
+    @Order(4)
     @DisplayName("Testing find by zipcode partial matching")
         public void testFindByZipcode() {
         List<Theater> matchingTheaters = theaterService.getTheatersByZipcode("19");
@@ -74,21 +70,33 @@ class TheaterServiceTest {
     }
 
     @Test
+    @Order(5)
     @DisplayName("Testing create theater")
-    public void t1TestCreateTheaterMethod() {
+    public void testCreateTheaterMethod() throws Exception {
         // We assume theater is created through the BeforeAll setup method.
-        Optional<Theater> theater = theaterService.getTheaterByTheaterId(9999999);
 
+        theaterService.saveTheater(theaterTestRec);
+        Optional<Theater> theater = theaterService.getTheaterByTheaterId(9999999);
         Assertions.assertTrue(theater.isPresent());
 
-        if (theater.isPresent()) {
-            Assertions.assertEquals("TestCity", theater.get().getLocation().getAddress().getCity());
-        }
+        theater.ifPresent(value -> Assertions.assertEquals("TestCity", value.getLocation().getAddress().getCity()));
     }
 
     @Test
+    @Order(6)
+    @DisplayName("Testing update theater method throws not exists exception")
+    public void testUpdateFunctionThrowsNoRecordExistsException() {
+        Optional<Theater> theater = theaterService.getTheaterByTheaterId(9999998);
+        Assertions.assertThrows(Exception.class,
+                () -> theaterService.updateTheater(theater.get(),9999998))
+        ;
+    }
+
+
+    @Test
+    @Order(7)
     @DisplayName("Testing update theater method in service")
-    public void t2TestUpdateFunction() throws Exception {
+    public void testUpdateFunction() throws Exception {
         Optional<Theater> theater = theaterService.getTheaterByTheaterId(9999999);
         Assertions.assertTrue(theater.isPresent());
 
@@ -100,8 +108,9 @@ class TheaterServiceTest {
     }
 
     @Test
+    @Order(8)
     @DisplayName("Testing delete theater method in service")
-    public void t3TestDeleteFunction() throws Exception {
+    public void testDeleteFunction() throws Exception {
         Optional<Theater> theater = theaterService.getTheaterByTheaterId(9999999);
         Assertions.assertTrue(theater.isPresent());
 
@@ -110,6 +119,14 @@ class TheaterServiceTest {
             theater = theaterService.getTheaterByTheaterId(9999999);
             Assertions.assertTrue(theater.isEmpty());
         }
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Test delete theater throws exception")
+    public void testDeleteTheaterThrowsException() {
+        Assertions.assertThrows(Exception.class,
+                () -> theaterService.deleteTheater(9999998));
     }
 
 }
